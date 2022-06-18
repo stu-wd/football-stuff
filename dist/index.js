946065c01d22a675f7af7b81148c387403dbb548
Stu Darsey <stuart.darsey@stratusgrid.com>
2022-06-18 14:44:22 -0400

    gitignore

--- dist/index.js
+++ dist/index.js
@@ -9,23 +9,20 @@ pitchers.map((pitcher) => {
     for (const gameId in starts) {
         if (starts[gameId] === 'PROBABLE') {
             const url = `https://www.espn.com/mlb/game?gameId=${gameId}`;
-            // x(url, '.pitchers__row', {
-            //   fullName: ['.fullName'],
-            // })((err: any, result: any) => {
-            //   if (err) return `ERROR ${err}`;
-            //   else {
-            //     console.log(gameId, result);
-            //   }
-            // });
             x(url, {
                 date: 'title',
-                pitchers: ['.pitchers__row .fullName']
+                pitchers: ['.pitchers__row .fullName'],
             })((err, result) => {
                 if (err)
                     return `ERROR ${err}`;
                 else if (result.pitchers.length > 0)
-                    console.log(gameId, result);
+                    formatEspnTitle(result.date, (data) => {
+                        console.log(data);
+                    });
             });
         }
     }
 });
+const formatEspnTitle = (date, callback) => {
+    const sliced = date.split(' - ');
+};
--- src/index.ts
+++ src/index.ts
@@ -13,23 +13,22 @@ pitchers.map((pitcher: Player) => {
   for (const gameId in starts) {
     if (starts[gameId] === 'PROBABLE') {
       const url = `https://www.espn.com/mlb/game?gameId=${gameId}`;
-      // x(url, '.pitchers__row', {
-      //   fullName: ['.fullName'],
-      // })((err: any, result: any) => {
-      //   if (err) return `ERROR ${err}`;
-      //   else {
-      //     console.log(gameId, result);
-      //   }
-      // });
       x(url, {
         date: 'title',
-        pitchers: ['.pitchers__row .fullName']
+        pitchers: ['.pitchers__row .fullName'],
       })((err: any, result: any) => {
         if (err) return `ERROR ${err}`;
-        else if (result.pitchers.length > 0) console.log(gameId, result);
+        else if (result.pitchers.length > 0) formatEspnTitle(result.date, (data: any) => {
+          console.log(data);
+        });
       });
     }
   }
 });
+
+const formatEspnTitle = (date: string, callback: Function) => {
+  const sliced = date.split(' - ');
+
+};
 
 export { };
