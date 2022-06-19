d5c80203a6328205c1a8c91f107d07e1c06f0375
Stu Darsey <stuart.darsey@stratusgrid.com>
2022-06-18 22:23:05 -0400

    removes duplicate games (if both away and home are on team)

--- dist/index.js
+++ dist/index.js
@@ -10,11 +10,12 @@ const allPitchersAction = async (pitchers) => {
         const probStarts = await Promise.resolve(figureOutStarts(pitcher));
         startsByPitcher.push({ name: pitcher.fullName, probStarts });
     }));
-    const allStarts = [];
+    let allStarts = [];
     startsByPitcher.filter((pitcher) => pitcher.probStarts.length > 0).map((pitcher) => pitcher.probStarts.sort((a, b) => a.jsDate - b.jsDate).forEach((start) => allStarts.push(start)));
     allStarts.sort((a, b) => {
         return a.jsDate - b.jsDate;
     });
+    allStarts = allStarts.filter((element, index, array) => index === array.findIndex((ele) => (ele.gameId === element.gameId)));
     return {
         startsByPitcher,
         allStarts
@@ -38,22 +39,43 @@ const figureOutStarts = async (pitcher) => {
             });
             if (xray.pitchers.length > 0) {
                 const words = xray.title.split(' - ');
-                let jsDate = new Date(words[2]);
+                const away = xray.pitchers[0];
+                const home = xray.pitchers[1];
+                const game = words[0] + ' - ' + words[2];
+                const jsDate = new Date(words[2]);
                 let dayTime = new Date(words[2]).toUTCString().slice(0, 5);
+                let count = 0;
                 if (xray.UTC) {
                     dayTime += new Date(xray.UTC).toLocaleString().split(', ')[1];
                 }
                 else {
                     dayTime += 'TBD';
                 }
+                // if (pitchers.filter((pitcher: any) => pitcher.fullName === away)) {
+                //   count += 1;
+                // }
+                // if (pitchers.filter((pitcher: any) => pitcher.fullName === home)) {
+                //   count += 1;
+                // }
+                // if (pitchers.find(({ fullName }: any) => fullName === home != undefined)) count += 1;
+                // if (pitchers.find(({ fullName }: any) => fullName === away != undefined)) count += 1;
+                if (pitchers.findIndex(pitcher => pitcher.fullName === home) != -1) {
+                    // console.log('HOME: ', pitcher.fullName, home, game);
+                    count += 1;
+                }
+                if (pitchers.findIndex(pitcher => pitcher.fullName === away) != -1) {
+                    // console.log('AWAY: ', pitcher.fullName, away, game);
+                    count += 1;
+                }
                 const crafted = {
-                    game: words[0] + ' - ' + words[2],
+                    game,
                     dayTime,
-                    away: xray.pitchers[0],
-                    home: xray.pitchers[1],
+                    away,
+                    home,
                     UTC: xray.UTC,
                     gameId,
-                    jsDate
+                    jsDate,
+                    count
                 };
                 probStarts.push(crafted);
             }
@@ -67,4 +89,6 @@ const formatResponse = (result) => {
     return result;
 };
 const myStarts = await allPitchersAction(pitchers);
-console.log(myStarts.allStarts);
+let startsCount = 0;
+myStarts.allStarts.map((start) => startsCount += start.count);
+console.log(startsCount, myStarts.allStarts.length);
--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,19 @@ const allPitchersAction = async (pitchers: any[]) => {
     startsByPitcher.push({ name: pitcher.fullName, probStarts });
   }));
 
-  const allStarts = [] as any[];
+  let allStarts = [] as any[];
 
   startsByPitcher.filter((pitcher: any) => pitcher.probStarts.length > 0).map((pitcher: any) => pitcher.probStarts.sort((a, b) => a.jsDate - b.jsDate).forEach((start: any) => allStarts.push(start)));
 
   allStarts.sort((a, b) => {
     return a.jsDate - b.jsDate;
   });
+
+  allStarts = allStarts.filter((element, index, array) => index === array.findIndex((ele) => (
+    ele.gameId === element.gameId
+  )));
+
+
   return {
     startsByPitcher,
     allStarts
@@ -45,21 +51,42 @@ const figureOutStarts = async (pitcher: Player) => {
 
       if (xray.pitchers.length > 0) {
         const words = xray.title.split(' - ');
-        let jsDate = new Date(words[2]);
+        const away = xray.pitchers[0];
+        const home = xray.pitchers[1];
+        const game = words[0] + ' - ' + words[2];
+        const jsDate = new Date(words[2]);
         let dayTime = new Date(words[2]).toUTCString().slice(0, 5);
+        let count = 0;
         if (xray.UTC) {
           dayTime += new Date(xray.UTC).toLocaleString().split(', ')[1];
         } else {
           dayTime += 'TBD';
         }
+        // if (pitchers.filter((pitcher: any) => pitcher.fullName === away)) {
+        //   count += 1;
+        // }
+        // if (pitchers.filter((pitcher: any) => pitcher.fullName === home)) {
+        //   count += 1;
+        // }
+        // if (pitchers.find(({ fullName }: any) => fullName === home != undefined)) count += 1;
+        // if (pitchers.find(({ fullName }: any) => fullName === away != undefined)) count += 1;
+        if (pitchers.findIndex(pitcher => pitcher.fullName === home) != -1) {
+          // console.log('HOME: ', pitcher.fullName, home, game);
+          count += 1;
+        }
+        if (pitchers.findIndex(pitcher => pitcher.fullName === away) != -1) {
+          // console.log('AWAY: ', pitcher.fullName, away, game);
+          count += 1;
+        }
         const crafted = {
-          game: words[0] + ' - ' + words[2],
+          game,
           dayTime,
-          away: xray.pitchers[0],
-          home: xray.pitchers[1],
+          away,
+          home,
           UTC: xray.UTC,
           gameId,
-          jsDate
+          jsDate,
+          count
         };
         probStarts.push(crafted);
       }
@@ -75,7 +102,11 @@ const formatResponse = (result: any) => {
 
 const myStarts = await allPitchersAction(pitchers);
 
-console.log(myStarts.allStarts);
+let startsCount = 0;
+myStarts.allStarts.map((start) => startsCount += start.count);
+
+console.log(startsCount, myStarts.allStarts.length);
+
 
 
 export { };
