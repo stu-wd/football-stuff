e4a9021f0fe709c93d0909fff9a18964f02c94d8
Stu Darsey <stuart.darsey@stratusgrid.com>
2022-06-18 21:18:48 -0400

    created a few functions, currently returning an array full of prob starts

--- dist/index.js
+++ dist/index.js
@@ -4,26 +4,52 @@ const x = xray();
 const team = await getTeam();
 const hitters = team.hitters;
 const pitchers = team.pitchers;
-pitchers.map((pitcher) => {
+const allPitchersAction = async (pitchers) => {
+    const myFutureStarts = [];
+    await Promise.all(pitchers.map(async (pitcher) => {
+        const probStarts = await Promise.resolve(figureOutStarts(pitcher));
+        myFutureStarts.push({ name: pitcher.fullName, probStarts });
+    }));
+    return myFutureStarts;
+};
+const figureOutStarts = async (pitcher) => {
+    const probStarts = [];
     const starts = pitcher.starterStatusByProGame;
     for (const gameId in starts) {
         if (starts[gameId] === 'PROBABLE') {
             const url = `https://www.espn.com/mlb/game?gameId=${gameId}`;
-            x(url, {
-                date: 'title',
+            const xray = await x(url, {
+                title: 'title',
+                UTC: '.game-status span@data-date',
                 pitchers: ['.pitchers__row .fullName'],
             })((err, result) => {
                 if (err)
                     return `ERROR ${err}`;
-                else if (result.pitchers.length > 0)
-                    formatEspnTitle(result.date, (newDate) => {
-                        console.log(newDate, result.pitchers);
-                    });
+                else
+                    return result;
             });
+            if (xray.pitchers.length > 0) {
+                const words = xray.title.split(' - ');
+                let date = undefined;
+                if (xray.UTC) {
+                    date = new Date(xray.UTC).toUTCString().slice(0, 5) + new Date(xray.UTC).toLocaleString();
+                }
+                const crafted = {
+                    game: words[0] + ' - ' + words[2],
+                    date: date,
+                    home: xray.pitchers[0],
+                    away: xray.pitchers[1]
+                };
+                probStarts.push(crafted);
+            }
         }
     }
-});
-const formatEspnTitle = (date, callback) => {
-    const sliced = date.split(' - ');
-    callback(sliced[0] + ' - ' + sliced[2]);
+    ;
+    return probStarts;
 };
+const formatResponse = (result) => {
+    result.date = new Date(result.UTC).toUTCString().slice(0, 5) + new Date(result.UTC).toLocaleString();
+    return result;
+};
+const myFutureStarts = await allPitchersAction(pitchers);
+myFutureStarts.map((start) => console.log(start));
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,65 @@
 import { Player, Team } from './models';
 import { getTeam } from './services/getTeam';
 import xray from 'x-ray';
-import { DateTime } from 'luxon';
 
 const x = xray();
 
 const team: Team = await getTeam();
 const hitters: Player[] = team.hitters;
-const pitchers: Player[] = team.pitchers;
+const pitchers: any[] = team.pitchers;
 
-pitchers.map((pitcher: Player) => {
+const allPitchersAction = async (pitchers: any[]) => {
+  const myFutureStarts = [] as any[];
+  await Promise.all(pitchers.map(async (pitcher: any) => {
+    const probStarts = await Promise.resolve(figureOutStarts(pitcher));
+    myFutureStarts.push({ name: pitcher.fullName, probStarts });
+  }));
+  return myFutureStarts;
+};
+
+const figureOutStarts = async (pitcher: Player) => {
+  const probStarts = [] as any[];
   const starts = pitcher.starterStatusByProGame;
   for (const gameId in starts) {
     if (starts[gameId] === 'PROBABLE') {
       const url = `https://www.espn.com/mlb/game?gameId=${gameId}`;
-      x(url, {
-        date: 'title',
+      const xray = await x(url, {
+        title: 'title',
+        UTC: '.game-status span@data-date',
         pitchers: ['.pitchers__row .fullName'],
       })((err: any, result: any) => {
         if (err) return `ERROR ${err}`;
-        else if (result.pitchers.length > 0) formatEspnTitle(result.date, (newDate: any) => {
-          console.log(newDate, result.pitchers);
-        });
+        else return result;
       });
-    }
-  }
-});
 
-const formatEspnTitle = (date: string, callback: Function) => {
-  const sliced = date.split(' - ');
-  callback(sliced[0] + ' - ' + sliced[2]);
+      if (xray.pitchers.length > 0) {
+        const words = xray.title.split(' - ');
+        let date = undefined;
+        if (xray.UTC) {
+          date = new Date(xray.UTC).toUTCString().slice(0, 5) + new Date(xray.UTC).toLocaleString();
+        }
+        const crafted = {
+          game: words[0] + ' - ' + words[2],
+          date: date,
+          home: xray.pitchers[0],
+          away: xray.pitchers[1]
+        };
+        probStarts.push(crafted);
+      }
+    }
+  };
+  return probStarts;
 };
+
+const formatResponse = (result: any) => {
+  result.date = new Date(result.UTC).toUTCString().slice(0, 5) + new Date(result.UTC).toLocaleString();
+  return result;
+};
+
+const myFutureStarts = await allPitchersAction(pitchers);
+
+myFutureStarts.map((start: any) => console.log(start));
+
+
 
 export { };
